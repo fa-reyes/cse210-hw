f485b70fcb5209a70a40717296f608e7779a97db
fa-reyes <fabian.andree2004@gmail.com>
2026-03-04 15:23:09 -0300

    Exercise 3 completed. Random number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string playAgain;
+        do
+        {
+            Random random = new Random();
+            int num = random.Next(1,101);
+
+            int userGuess = -1;
+            int numberOfGuesses = 0;
+
+            while (userGuess != num)
+            {
+                Console.Write("Guess a number from 1 to 100: ");
+                userGuess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+                if (userGuess < num)
+                {
+                    Console.WriteLine("Go higher!");
+                }
+                else if (userGuess > num)
+                {
+                    Console.WriteLine("Go lower!");
+                }
+                else if (userGuess == num)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberOfGuesses} attempts.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Enter a valid number.");
+                }
+            }
+
+            Console.Write("Do you want to play again? Yes/No: ");
+            playAgain = Console.ReadLine().ToLower();
+            
+        } while (playAgain == "yes");
+        Console.WriteLine("Thanks for playing!");
+
     }
 }
