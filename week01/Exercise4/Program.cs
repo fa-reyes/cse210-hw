74846f4ac7fbe3d39922c8c75d777742d063f852
fa-reyes <fabian.andree2004@gmail.com>
2026-03-06 15:51:25 -0300

    Exercise 4 completed. Sum, average, smallest and largest number calculator.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumbers = -1;
+        int numSum = 0;
+        double numAv = 0;
+        int largeNum = int.MinValue;
+        int smallNum = int.MaxValue;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userNumbers != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumbers = int.Parse(Console.ReadLine());
+
+            if (userNumbers != 0)
+            {
+                numbers.Add(userNumbers);
+                numSum = numSum + userNumbers;
+
+                if (largeNum < userNumbers)
+                {
+                    largeNum = userNumbers;
+                }
+
+                if (userNumbers > 0)
+                {
+                    if (smallNum > userNumbers)
+                    {
+                        smallNum = userNumbers;
+                    }
+                }
+            }
+        }
+
+        if (numbers.Count() > 0)
+        {
+            numAv = (double)numSum / numbers.Count;
+
+            Console.WriteLine($"The sum is: {numSum}");
+            Console.WriteLine($"The average is: {numAv:F2}");
+            Console.WriteLine($"The largest number is: {largeNum}");
+            Console.WriteLine($"The smallest positive number is: {smallNum}");
+
+            numbers.Sort();
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine($"The sorted list is: {num}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+
+        
     }
 }
