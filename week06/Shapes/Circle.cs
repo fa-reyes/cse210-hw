156f07c5c0a429ef97ea0202b7a96319342ed43c
fa-reyes <fabian.andree2004@gmail.com>
2026-04-08 19:29:43 -0400

    Shapes program completed. Polymorphism applied

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,19 @@
+public class Circle : Shape
+{
+    private double _radius = 0;
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * (_radius * _radius);
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s1 = new Square();
+        s1.SetColor("red");
+        s1.SetSide(5);
+
+        Rectangle r1 = new Rectangle();
+        r1.SetColor("yellow");
+        r1.SetLength(8);
+        r1.SetWidth(4);
+
+        Circle c1 = new Circle();
+        c1.SetColor("orange");
+        c1.SetRadius(3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
